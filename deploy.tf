4bcd7f918c522bb8764bf8db231e1f91d18441b2
fred aklamanu <fred.aklamanu@nokia-bell-labs.com>
2020-11-21 11:28:59 +0100

    iam+ec2

--- deploy.tf
+++ deploy.tf
@@ -44,10 +44,6 @@ resource "aws_iam_user_login_profile" "freddyflo-profile" {
 }
 
 
-# generated with keybase
-output "password" {
-  value = aws_iam_user_login_profile.freddyflo-profile.encrypted_password
-}
 
 # fetch existing group admin
 data "aws_iam_group" "admin-group" {
@@ -64,5 +60,121 @@ resource "aws_iam_user_group_membership" "membership-admin" {
 }
 
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# default vpc
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+
+# security group
+resource "aws_security_group" "ssh_web" {
+  name = "ssh_web"
+  description = "Allow standard http and http ports inbound and everything outbound"
+  
+
+  # inbound traffic
+  ingress  {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  } 
+  ingress  {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress  {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }  
+
+  # outbound traffic
+  egress   {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  } 
+
+  tags = {
+    "Terraform": "true"
+  }
+  
+}
+
+
+
+# generate key pair
+resource "tls_private_key" "test_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "fred-key"
+  #public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDB6yRGs2Y0Jn2Po8gG6gR8NgeEDq6fBgWchUZRcEgdhqEgm7FOSfVJto2egzk/yPQchp20WMMx8SOleU0EOCG/xT772EXY+abBfbFnLOByAkKK0eQYKrdWn9ZObefcMKVEOyifpaEhaKI6yuXYGispKI14OPQrhP+UV0Bo4yoZ5xkTlFg12rY7E+KkkJS54xLKouGmSncSTQ1AIIrAOOfy6pWKDZJbS9qdPfSljwNL7+XRkvq1Bm7YCu2hsfGw35QN1RPqbTtbho+u01v9uJFV2nMCCRxv/C2GZx1Z+HKp8gu7YfdTD8YcjToLemQEUnDjv3yQeN8o55/hE7HRok/9 fred@fred-ThinkPad-P43s"
+  public_key = tls_private_key.test_key.public_key_openssh
+}
+
+resource "aws_instance" "web" {
+  ami              = data.aws_ami.ubuntu.id
+  instance_type    = "t3.micro"
+
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.ssh_web.id]
+  key_name                    = aws_key_pair.generated_key.key_name
+  user_data                   = <<-EOF
+                                #!/bin/bash
+                                apt-get update -y
+                                apt install apache2 -y
+                                echo "Welcome to My First EC2 Instance Web Server" > /var/www/html/index.html
+                                service http start
+                                EOF
+ 
+  tags = {
+    Name = "TestSSH"
+  }
+}
+
+
+# generated with keybase
+output "password" {
+  value = aws_iam_user_login_profile.freddyflo-profile.encrypted_password
+}
+
+# get private ssh key
+output "private_ssh_key" {
+  value = tls_private_key.test_key.private_key_pem
+
+}
+
+output "instance_ip" {
+  description = "The public ip for ssh access"
+  value       = aws_instance.web.public_ip
+}
+
+# subnet - availability zone
 
 
